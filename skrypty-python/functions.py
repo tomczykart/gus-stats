cf6da8fde647bca33c70b090987464d6725df7ec
tomczykart <tomczyk.art@gmail.com>
2022-08-13 10:08:49 +0200

    New function to create dataframe from json

--- skrypty-python/functions.py
+++ skrypty-python/functions.py
@@ -1,16 +1,33 @@
 import pathlib
 import json
 import requests
+import pandas as pd
+
 
 #saving function
-def save_file(file_name,data):
-    p = pathlib.Path(__file__).parent.joinpath('data_import')
+def save_file(file_name,folder_name,data):
+    p = pathlib.Path(__file__).parent.joinpath(folder_name)
     p.mkdir(exist_ok = True)
     with open(p.joinpath(file_name), 'w') as f:
         json.dump(data.json(),f)
+
 
 #get data from gus, json format
 def get_data(variable):
     #gathering data
     response = requests.get(f"https://bdl.stat.gov.pl/api/v1/data/by-variable/{variable}?unit-level=2&lang=pl&format=json")
     return response
+
+
+#read json and convert it to pandas dataframe table
+def generate_table(file_name):
+    #read json
+    with open(file_name) as json_data:
+        gus_data = json.load(json_data)
+
+    #retrive results, flatten nested json
+    gus_data = gus_data['results']
+    gus_data = pd.json_normalize(gus_data,'values',['name'])
+    #pivot the table
+    gus_data = gus_data.pivot_table(values='val', index='name', columns='year')
+    return gus_data
--- skrypty-python/project-gus.py
+++ skrypty-python/project-gus.py
@@ -1,13 +1,16 @@
-from functions import save_file, get_data
+from functions import save_file, get_data, generate_table
 import json
 
 #numbers of variables gathereded from gus api
 variables = [747340, 747335, 747330, 747595, 747500, 747505, 747515, 747520, 747525, 747530, 747535, 747540]
 
-for variable in variables:
-
+#scratch json from web
+#for variable in variables:
     #assign data
-    data = get_data(variable)
-
+    #data = get_data(variable)
     #save data to file
-    save_file(f"data_v_{variable}.json",data);
+    #save_file(f'data_v_{variable}.json','data_import',data);
+    #generate table
+
+table = generate_table(f'\data_import\data_v_{variables[1]}.json')
+print(table)
