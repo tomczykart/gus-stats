53cf78e5d7dce925c6f9378f1c1cda84ab29809a
tomczykart <tomczyk.art@gmail.com>
2022-08-14 20:50:22 +0200

    Delete junk files

--- skrypty-python/project-gus.py
+++ skrypty-python/project-gus.py
@@ -6,12 +6,12 @@ import json
 variables = [747340, 747335, 747330, 747595, 747500, 747505, 747515, 747520, 747525, 747530, 747535, 747540]
 
 #scratch json from web
-#for variable in variables:
-    #assign data
-    #data = get_data(variable)
-    #save data to file
-    #save_file(f'data_v_{variable}.json','data_import',data);
-    #generate table
+for variable in variables:
+    assign data
+    data = get_data(variable)
+    save data to file
+    save_file(f'data_v_{variable}.json','data_import',data);
+    generate table
 
 #read data from json to table frame, show results
 table = generate_table(f'\data_import\data_v_{variables[10]}.json')
